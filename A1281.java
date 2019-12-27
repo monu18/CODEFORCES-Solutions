a2574c5abb4df328d10f0a1d70fe3436e82156e2
monu18 <mozis.bot@gmail.com>
2019-12-27 18:35:11 +0530

    Atom

--- A1281.java
+++ A1281.java
@@ -1,15 +1,15 @@
 import java.lang.*;
 import java.util.*;
-public class A1281 {
+public class A12281 {
 
-	static String swap(String str, int i, int j) 
-    { 
-        char ch[] = str.toCharArray(); 
-        char temp = ch[i]; 
-        ch[i] = ch[j]; 
-        ch[j] = temp; 
-        return ch.toString(); 
-    } 
+	static String swap(String str, int i, int j)
+    {
+        char ch[] = str.toCharArray();
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+        return new String(ch);
+    }
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt();
@@ -21,20 +21,21 @@ public class A1281 {
 			char temp[]=s11.toCharArray();
 			Arrays.sort(temp);
 			s11=new String(temp);
+			if (s1.compareTo(s2)<0) {
+				System.out.println(s1);
+				continue;
+			}
 			boolean flag=false;
 			for(int i=0;i<s1.length();i++) {
 				if (s1.charAt(i)>s11.charAt(i)) {
-					for(int j=i+1;j<s1.length();j++) {
-						s1=swap(s1,i,j);
-						if (s1.compareTo(s2)>0) {
-							for (int j2 = 0; j2 < s1.length(); j2++) {
-								System.out.print(s1.charAt(j2));
-							}
-							System.out.println(s1);
+					for(int j=s1.length()-1;j>i;j--) {
+						//s1=swap(s1,i,j);
+						//System.out.println("s1 "+s1);
+						if (s1.charAt(j)==s11.charAt(i)) {
+							s1=swap(s1,i,j);
 							flag=true;
 							break;
 						}
-						s1=swap(s1,i,j);
 					}
 				}
 				if (flag) {
@@ -43,11 +44,21 @@ public class A1281 {
 			}
 			if (flag==false) {
 				System.out.println("---");
-				
+
 			}
-			
+			else
+			{
+				if (s1.compareTo(s2)<0) {
+					System.out.println(s1);
+				}
+				else
+				{
+					System.out.println("---");
+				}
+			}
+
 		}
-		
+
 	}
 
 }
