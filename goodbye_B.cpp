f3528a52afef97fa373f1809f8d5676e951b675c
monu18 <mozis.bot@gmail.com>
2019-12-29 23:21:50 +0530

    Atom

---
+++ goodbye_B.cpp
@@ -0,0 +1,261 @@
+//mOzis_
+/////*31022618*/////
+//****//MONU KUMAR\\****//
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define tc ll t=1; cin>>t; while(t--)
+#define ll long long int
+#define ull unsigned long long
+#define p1(x) cout <<x <<"\n";
+#define p2(x,y) cout <<x <<" " <<y <<"\n";
+#define p3(x,y,z) cout <<x <<" " <<y <<" " <<z <<"\n";
+#define nL cout<<"\n";
+#define lp(i,l,r) for (long long int i = l; i < r; i++)
+#define lpr(i,r,l) for (long long int i = r-1; i >=l ; i--)
+#define mem(a,x) memset(a,x,sizeof(a))              //set elements of array to some value
+#define pi 3.1415926535897932384626
+#define mod 1000000007
+#define vi vector<int>
+#define vii vector< vector<long long int> >
+#define vpi vector< pair<long long int,long long int> >
+#define mp(a, b) make_pair(a, b)
+#define pb(x) push_back(x)
+#define ff first
+#define ss second
+#define siz 256
+#define leadzero(a) __builtin_clz(a)        // count leading zeroes
+#define trailzero(a) __builtin_ctz(a)       // count trailing zeroes
+#define bitcount(a) __builtin_popcount(a)   // count set bits
+#define lps(v,x) (lower_bound(all(v),x)-v.begin())
+#define ups(v,x) (upper_bound(all(v),x)-v.begin())
+//#########################################################################################
+bool anagram(char *s1, char *s2)
+{
+	ll count[siz];
+    mem(count,0) ;
+    ll i;
+    for (i = 0; s1[i] && s2[i];  i++)
+    {
+        count[s1[i]]++;
+        count[s2[i]]--;
+    }
+    if (s1[i] || s2[i])
+    {
+      return false;
+    }
+    lp(i,0,siz)
+    {
+    	 if (count[i])
+    	 {
+    	 	 return false;
+    	 }
+    }
+     return true;
+
+}
+//###########################################################################################
+int find_largest_digit(int n)
+{
+    int mx = 0 ;
+    while(n>0)
+    {
+        mx = max(mx, n%10) ;
+        n/=10 ;
+    }
+    return mx  ;
+}
+//###########################################################################################
+ll no_of_digits(ll n)
+{
+    ll ans = 0 ;
+    while(n>0)
+    {
+        ans++ ;
+        n/=10 ;
+    }
+    return ans ;
+}
+//###########################################################################################
+// modulo Multiplication
+ll moduloMultiplication(ll  a, ll  b,  ll  zz)
+{
+    ll res = 0;
+    a %= mod;
+    while (b)
+    {
+        if (b & 1)
+            res = (res + a) % zz;
+
+        a = (2 * a) % zz;
+
+        b >>= 1;
+    }
+    return res;
+}
+//###########################################################################################
+
+ll convert(string s)
+{
+    bool o = true ;
+    ll ans = 0 ;
+    for(ll i=0;i<s.length();i++)
+    {
+        ans = ans*10 + (s[i]-48) ;
+    }
+    return ans ;
+}
+//###########################################################################################
+//function to calculate a^b under mod...
+ll powmod(ll a,ll b, ll modulo)
+{
+    if(b==0 || a==1) return 1;
+    ll half=powmod(a,(b/2),modulo) % modulo;
+    ll full = (half*half)%modulo;
+
+    if(b%2) return (full*a)%modulo;
+    return full%modulo;
+}
+//###########################################################################################
+//function to calculate inverse modulo under mod...assuming gcd is 1
+ll invmod(ll a,ll modulo)
+{
+    ll check=powmod(a,modulo-2,modulo) %modulo;
+    return check;
+}
+//###########################################################################################
+
+ll max(ll a,ll b)
+{
+  if (a>b) {
+    /* code */
+    return a;
+  }
+  return b;
+}
+//###########################################################################################
+ll min(ll a,ll b)
+{
+  if (a<b) {
+    /* code */
+    return a;
+  }
+  return b;
+}
+//###########################################################################################
+ll min(ll a,ll b, ll c)
+{
+  if (a<b) {
+    /* code */
+    return min(a,c);
+  }
+	else
+	{
+			return min(b,c);
+	}
+
+}
+//###########################################################################################
+ll max(ll a,ll b, ll c)
+{
+  if (a<b) {
+    /* code */
+    return max(b,c);
+  }
+	else
+	{
+			return min(a,c);
+	}
+
+}
+//###########################################################################################
+ll min(ll a,int b)
+{
+	if (a<b) return a;
+	return b;
+}
+//###########################################################################################
+ll min(int a,ll b)
+{
+	if (a<b) return a;
+	return b;
+}
+//###########################################################################################
+ll max(ll a,int b)
+{
+	if (a>b) return a;
+	return b;
+}
+//###########################################################################################
+ll max(int a,ll b)
+{
+	if (a>b) return a;
+	return b;
+}
+//###########################################################################################
+ll gcd(ll a,ll b)
+{
+	if (b==0) return a;
+	return gcd(b,a%b);
+}
+//###########################################################################################
+ll lcm(ll a,ll b)
+{
+	return a/gcd(a,b)*b;
+}
+//###########################################################################################
+void yes()
+{
+  cout<<"YES"<<"\n";
+}
+//###########################################################################################
+void no()
+{
+  cout<<"NO"<<"\n";
+}
+//###########################################################################################
+int main()
+{
+//	fast
+//	clock_t launch=clock();
+	// string r = s1.substr(1, 3);
+	//while(clock()<CLOCKS_PER_SEC*10)
+	//using num = modnum<int(1e9)+7>;
+ //num len = s.size();
+	tc
+	{
+
+		ll n;
+    cin>>n;
+    ll a[n];
+    ll maxx=0,minn=1000000000;
+    ll max_ind=0,min_ind=0;
+    map<ll,vector<int> >m;
+    for (size_t i = 0; i < n; i++) {
+      /* code */
+      cin>>a[i];
+
+    }
+		ll flag=0;
+		for (size_t i = 0; i < n-1; i++) {
+			/* code */
+			if (abs(a[i]-a[i+1])==2) {
+				/* code */
+				yes();
+				cout<<i+1<<" "<<i+2<<"\n";
+				flag=1;
+				break;
+			}
+		}
+		if (flag==0) {
+			/* code */
+			no();
+		}
+
+
+	}//tc
+
+	//clog<<((long double)(clock()-launch)/CLOCKS_PER_SEC)<<"\n";
+	return 0;
+}
+//////////*********************end of program*********************//////////
