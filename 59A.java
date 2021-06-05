d35c6efa8a6cdfdd36e243bc08cd20f6ebaf2375
Monu Kumar <mozis.bot@gmail.com>
2021-06-05 12:16:04 +0530

    Atom

---
+++ 59A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class xx{
+
+  public static void main(String args) {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    String s1,s2;
+    s1=s.toLowerCase();
+    s2=s.toUpperCase();
+    for (int i=0;i<s.length() ;i++ ) {
+
+    }
+  }
+
+}
